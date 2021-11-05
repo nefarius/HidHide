8393a0e248dc0ae593096a8c38eedb42b0617cea
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-11-05 14:02:47 +0100

    Converted utility method from string to wide string

--- HidHideCLI/src/Commands.cpp
+++ HidHideCLI/src/Commands.cpp
@@ -13,23 +13,24 @@
 namespace
 {
     // https://stackoverflow.com/a/33799784
-    std::string escape_json(const std::string &s) {
-        std::ostringstream o;
-        for (auto c = s.cbegin(); c != s.cend(); c++) {
-            switch (*c) {
-            case '"': o << "\\\""; break;
-            case '\\': o << "\\\\"; break;
-            case '\b': o << "\\b"; break;
-            case '\f': o << "\\f"; break;
-            case '\n': o << "\\n"; break;
-            case '\r': o << "\\r"; break;
-            case '\t': o << "\\t"; break;
+    std::wstring escape_json(const std::wstring &s) {
+        std::wostringstream o;
+        for (const wchar_t c : s)
+        {
+            switch (c) {
+            case L'"': o << L"\\\""; break;
+            case L'\\': o << L"\\\\"; break;
+            case L'\b': o << L"\\b"; break;
+            case L'\f': o << L"\\f"; break;
+            case L'\n': o << L"\\n"; break;
+            case L'\r': o << L"\\r"; break;
+            case L'\t': o << L"\\t"; break;
             default:
-                if ('\x00' <= *c && *c <= '\x1f') {
+                if (L'\x00' <= c && c <= L'\x1f') {
                     o << "\\u"
-                      << std::hex << std::setw(4) << std::setfill('0') << (int)*c;
+                      << std::hex << std::setw(4) << std::setfill(L'0') << static_cast<int>(c);
                 } else {
-                    o << *c;
+                    o << c;
                 }
             }
         }
