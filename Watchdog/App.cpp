e6e726dac19c196dd24f88ccfa37b76dc4056355
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-28 23:03:41 +0200

    Update App.cpp

--- Watchdog/App.cpp
+++ Watchdog/App.cpp
@@ -1,6 +1,8 @@
 #include "App.hpp"
 #include <fstream>
 #include "Util.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/msvc_sink.h>
 
 void App::initialize(Application& self)
 {
@@ -14,10 +16,23 @@ void App::uninitialize()
 
 int App::main(const std::vector<std::string>& args)
 {
-    std::ofstream file(R"(D:\StartUp_Skaner\Projekty\poco-windows-service\log.txt)");
-    file << "The service is running";
-    file.flush();
-    file.close();
+    auto debugSink = std::make_shared<spdlog::sinks::msvc_sink_mt>(false);
+#if _DEBUG
+    debugSink->set_level(spdlog::level::debug);
+#else
+    debugSink->set_level(spdlog::level::info);
+#endif
+
+    auto logger = std::make_shared<spdlog::logger>("HidHideWatchdog", debugSink);
+
+#if _DEBUG
+    spdlog::set_level(spdlog::level::debug);
+    logger->flush_on(spdlog::level::debug);
+#else
+			logger->flush_on(spdlog::level::info);
+#endif
+
+    spdlog::set_default_logger(logger);
 
     while (true)
     {
