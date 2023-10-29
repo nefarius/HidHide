240bb99f631d09cf80aa7c6218cc6f6fc41617ff
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-29 18:25:29 +0100

    Updated path to signtool to latest SDK

--- stage1.ps1
+++ stage1.ps1
@@ -1,4 +1,4 @@
-$signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe"
+$signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
 $timestampUrl = "http://timestamp.digicert.com"
 $certName = "Nefarius Software Solutions e.U."
 
