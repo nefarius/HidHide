3dc94acac87f4b79984642a3d0cfed650c8e3f5c
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-05-11 18:56:34 +0200

    Delete stage1.ps1

--- stage1.ps1
+++
@@ -1,8 +0,0 @@
-$signTool = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe"
-$timestampUrl = "http://timestamp.digicert.com"
-$certName = "Nefarius Software Solutions e.U."
-
-$files =    "`".\bin\Release\x64\*.exe`" "
-
-# sign with adding to existing certificates
-Invoke-Expression "& `"$signTool`" sign /v /as /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
