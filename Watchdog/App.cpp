a6a5ad5cde16f656b1d32ce06c71eda7cba6eab7
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-02 17:48:20 +0200

    Refactored watchdog service (#168)
    * Bumped to C++23
    
    * Moved stuff to neflib
    
    * Update App.cpp
    
    * Update appveyor.yml
    
    * Vcpkg adjustments
    
    * Update appveyor.yml
    
    * Update App.cpp
    
    * Fixes
    
    * Update vcpkg-configuration.json
    
    * Update vcpkg-configuration.json
    
    * Added app icon to watchdog
    
    * Update vcpkg-configuration.json
    
    * Update vcpkg-configuration.json
    
    * Update App.cpp
    
    * Update vcpkg-configuration.json
    
    * Update vcpkg-configuration.json
    
    * Update vcpkg-configuration.json
    
    * Update vcpkg-configuration.json
    
    * Updated dependencies

--- Watchdog/App.cpp
+++ Watchdog/App.cpp
@@ -1,7 +1,13 @@
 #include "App.hpp"
-#include "Util.h"
 #include <initguid.h>
 #include <devguid.h>
+
+#include <variant>
+#include <expected>
+#include <format>
+
+#include <nefarius/neflib/MiscWinApi.hpp>
+#include <nefarius/neflib/ClassFilter.hpp>
 
 #pragma warning(disable: 26800)
 #include <spdlog/spdlog.h>
@@ -27,67 +33,6 @@ class WatchdogTask : public Poco::Task
 {
     bool _isInteractive;
 
-    static DWORD CheckServiceStatus(const std::wstring& serviceName, unsigned long& serviceState)
-    {
-        SC_HANDLE sch = nullptr;
-        SC_HANDLE svc = nullptr;
-        DWORD error = ERROR_SUCCESS;
-
-        __try
-        {
-            sch = OpenSCManager(
-                nullptr,
-                nullptr,
-                SC_MANAGER_ALL_ACCESS
-            );
-            if (sch == nullptr)
-            {
-                error = GetLastError();
-                __leave;
-            }
-
-            svc = OpenService(
-                sch,
-                serviceName.c_str(),
-                SC_MANAGER_ALL_ACCESS
-            );
-            if (svc == nullptr)
-            {
-                error = GetLastError();
-                __leave;
-            }
-
-            SERVICE_STATUS_PROCESS stat;
-            DWORD needed = 0;
-            BOOL ret = QueryServiceStatusEx(
-                svc,
-                SC_STATUS_PROCESS_INFO,
-                (BYTE*)&stat,
-                sizeof stat,
-                &needed
-            );
-            if (ret == 0)
-            {
-                error = GetLastError();
-                __leave;
-            }
-
-            serviceState = stat.dwCurrentState;
-
-            error = ERROR_SUCCESS;
-            __leave;
-        }
-        __finally
-        {
-            if (svc)
-                CloseServiceHandle(svc);
-            if (sch)
-                CloseServiceHandle(sch);
-        }
-
-        return error;
-    }
-
 public:
     explicit WatchdogTask(const std::string& name, const bool isInteractive)
         : Task(name)
@@ -105,71 +50,95 @@ public:
 
         do
         {
-            const auto serviceName = L"HidHide";
-            unsigned long winError = 0, serviceStatus = 0;
+            const std::wstring serviceName = L"HidHide";
+
+            const auto serviceStatus = nefarius::winapi::services::GetServiceStatus(serviceName);
 
             // check if driver service is healthy
-            if ((winError = CheckServiceStatus(serviceName, serviceStatus)) != ERROR_SUCCESS)
+            if (!serviceStatus)
             {
-                spdlog::error("Failed to query service status, error {}", winError);
+                logger->error("Failed to query service status, error {}", serviceStatus.error().getErrorMessageA());
                 continue;
             }
 
             // expecting service to be running as an indicator that driver is loaded
-            if (serviceStatus != SERVICE_RUNNING)
+            if (serviceStatus.value().dwCurrentState != SERVICE_RUNNING)
             {
-                spdlog::error("Driver service not detected running, removing filter entries");
+                logger->error("Driver service not detected running, removing filter entries");
 
                 //
                 // Prevents bricked HID devices
                 // 
 
-                RemoveDeviceClassFilter(&GUID_DEVCLASS_HIDCLASS,
-                                        serviceName, util::DeviceClassFilterPosition::Upper);
-                RemoveDeviceClassFilter(&GUID_DEVCLASS_XNACOMPOSITE,
-                                        serviceName, util::DeviceClassFilterPosition::Upper);
-                RemoveDeviceClassFilter(&GUID_DEVCLASS_XBOXCOMPOSITE,
-                                        serviceName, util::DeviceClassFilterPosition::Upper);
+                auto removeResult = RemoveDeviceClassFilter(&GUID_DEVCLASS_HIDCLASS,
+                                                            serviceName,
+                                                            nefarius::devcon::DeviceClassFilterPosition::Upper);
+
+                if (!removeResult)
+                {
+                    logger->error("Removal from GUID_DEVCLASS_HIDCLASS failed with {}",
+                                  removeResult.error().getErrorMessageA());
+                }
+
+                removeResult = RemoveDeviceClassFilter(&GUID_DEVCLASS_XNACOMPOSITE,
+                                                       serviceName,
+                                                       nefarius::devcon::DeviceClassFilterPosition::Upper);
+
+                if (!removeResult)
+                {
+                    logger->error("Removal from GUID_DEVCLASS_XNACOMPOSITE failed with {}",
+                                  removeResult.error().getErrorMessageA());
+                }
+
+                removeResult = RemoveDeviceClassFilter(&GUID_DEVCLASS_XBOXCOMPOSITE,
+                                                       serviceName,
+                                                       nefarius::devcon::DeviceClassFilterPosition::Upper);
+
+                if (!removeResult)
+                {
+                    logger->error("Removal from GUID_DEVCLASS_XBOXCOMPOSITE failed with {}",
+                                  removeResult.error().getErrorMessageA());
+                }
 
                 continue;
             }
 
             // filter value or entry not present
-            if (bool found = false; !HasDeviceClassFilter(&GUID_DEVCLASS_HIDCLASS, serviceName,
-                                                          util::DeviceClassFilterPosition::Upper, found) || !found)
+            if (!HasDeviceClassFilter(&GUID_DEVCLASS_HIDCLASS, serviceName,
+                                      nefarius::devcon::DeviceClassFilterPosition::Upper).value_or(false))
             {
-                spdlog::warn("Filter missing for HIDClass, adding");
+                logger->warn("Filter missing for HIDClass, adding");
 
                 if (!AddDeviceClassFilter(&GUID_DEVCLASS_HIDCLASS, serviceName,
-                                          util::DeviceClassFilterPosition::Upper))
+                                          nefarius::devcon::DeviceClassFilterPosition::Upper))
                 {
-                    spdlog::error("Failed to add upper filters entry for HIDClass");
+                    logger->error("Failed to add upper filters entry for HIDClass");
                 }
             }
 
             // filter value or entry not present
-            if (bool found = false; !HasDeviceClassFilter(&GUID_DEVCLASS_XNACOMPOSITE, serviceName,
-                                                          util::DeviceClassFilterPosition::Upper, found) || !found)
+            if (!HasDeviceClassFilter(&GUID_DEVCLASS_XNACOMPOSITE, serviceName,
+                                      nefarius::devcon::DeviceClassFilterPosition::Upper).value_or(false))
             {
-                spdlog::warn("Filter missing for XnaComposite, adding");
+                logger->warn("Filter missing for XnaComposite, adding");
 
                 if (!AddDeviceClassFilter(&GUID_DEVCLASS_XNACOMPOSITE, serviceName,
-                                          util::DeviceClassFilterPosition::Upper))
+                                          nefarius::devcon::DeviceClassFilterPosition::Upper))
                 {
-                    spdlog::error("Failed to add upper filters entry for XnaComposite");
+                    logger->error("Failed to add upper filters entry for XnaComposite");
                 }
             }
 
             // filter value or entry not present
-            if (bool found = false; !HasDeviceClassFilter(&GUID_DEVCLASS_XBOXCOMPOSITE, serviceName,
-                                                          util::DeviceClassFilterPosition::Upper, found) || !found)
+            if (!HasDeviceClassFilter(&GUID_DEVCLASS_XBOXCOMPOSITE, serviceName,
+                                      nefarius::devcon::DeviceClassFilterPosition::Upper).value_or(false))
             {
-                spdlog::warn("Filter missing for XboxComposite, adding");
+                logger->warn("Filter missing for XboxComposite, adding");
 
                 if (!AddDeviceClassFilter(&GUID_DEVCLASS_XBOXCOMPOSITE, serviceName,
-                                          util::DeviceClassFilterPosition::Upper))
+                                          nefarius::devcon::DeviceClassFilterPosition::Upper))
                 {
-                    spdlog::error("Failed to add upper filters entry for XboxComposite");
+                    logger->error("Failed to add upper filters entry for XboxComposite");
                 }
             }
         }
@@ -211,9 +180,11 @@ int App::main(const std::vector<std::string>& args)
 
     console->info("Application started");
 
-    const bool isAdmin = util::IsAdmin();
+#if !defined(_DEBUG)
+    const auto isAdmin = nefarius::winapi::security::IsAppRunningAsAdminMode();
 
-    if (isAdmin)
+    if (isAdmin.value_or(false))
+#endif
     {
         Poco::TaskManager tm;
         tm.start(new WatchdogTask("HidHideWatchdog", this->isInteractive()));
@@ -221,12 +192,18 @@ int App::main(const std::vector<std::string>& args)
         tm.cancelAll();
         tm.joinAll();
     }
+#if !defined(_DEBUG)
     else
     {
         errLogger->error("App need administrative permissions to run");
     }
+#endif
 
     console->info("Exiting application");
 
+#if !defined(_DEBUG)
     return isAdmin ? EXIT_OK : EXIT_TEMPFAIL;
+#else
+    return EXIT_OK;
+#endif
 }
