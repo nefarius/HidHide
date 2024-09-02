564e4f1839f8afc17f526d79c8e786d42b609a3d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-02 20:10:45 +0200

    Added version helper config

--- HidHideInstaller/InstallScript.cs
+++ HidHideInstaller/InstallScript.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 using WixSharp;
 using WixSharp.Forms;
 
-namespace Nefarius.HidHide.Installer
+namespace Nefarius.HidHide.Setup
 {
     internal class InstallScript
     {
