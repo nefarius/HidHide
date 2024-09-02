2a49b6eb491ac7d1e32f1c2391ff4bbd288eae20
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-02 20:07:53 +0200

    Added new WiX v4/5 based setup template

--- HidHideInstaller/InstallScript.cs
+++ HidHideInstaller/InstallScript.cs
@@ -3,65 +3,63 @@ using System.Windows.Forms;
 using WixSharp;
 using WixSharp.Forms;
 
-namespace Nefarius.HidHide.Setup
+namespace Nefarius.HidHide.Installer
 {
-	internal class InstallScript
-	{
-		static void Main()
-		{
-			var project = new ManagedProject("MyProduct",
-							 new Dir(@"%ProgramFiles%\My Company\My Product",
-								 new File("InstallScript.cs")));
+    internal class InstallScript
+    {
+        static void Main()
+        {
+            var project = new ManagedProject("MyProduct",
+                             new Dir(@"%ProgramFiles%\My Company\My Product",
+                                 new File("InstallScript.cs")));
 
-			project.Platform = Platform.x64;
+            project.GUID = new Guid("57F3646E-E9AB-4AA5-BD4E-2A4E01DDAD94");
 
-			project.GUID = new Guid("FE535A74-4501-40CA-9E9E-8DCA6B122095");
+            project.ManagedUI = ManagedUI.Empty;    //no standard UI dialogs
+            project.ManagedUI = ManagedUI.Default;  //all standard UI dialogs
 
-			project.ManagedUI = ManagedUI.Empty;    //no standard UI dialogs
-			project.ManagedUI = ManagedUI.Default;  //all standard UI dialogs
+            //custom set of standard UI dialogs
+            project.ManagedUI = new ManagedUI();
 
-			//custom set of standard UI dialogs
-			project.ManagedUI = new ManagedUI();
+            project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
+                                            .Add(Dialogs.Licence)
+                                            .Add(Dialogs.SetupType)
+                                            .Add(Dialogs.Features)
+                                            .Add(Dialogs.InstallDir)
+                                            .Add(Dialogs.Progress)
+                                            .Add(Dialogs.Exit);
 
-			project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
-											.Add(Dialogs.Licence)
-											.Add(Dialogs.SetupType)
-											.Add(Dialogs.Features)
-											.Add(Dialogs.InstallDir)
-											.Add(Dialogs.Progress)
-											.Add(Dialogs.Exit);
+            project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
+                                           .Add(Dialogs.Features)
+                                           .Add(Dialogs.Progress)
+                                           .Add(Dialogs.Exit);
 
-			project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
-										   .Add(Dialogs.Features)
-										   .Add(Dialogs.Progress)
-										   .Add(Dialogs.Exit);
+            project.Load += Msi_Load;
+            project.BeforeInstall += Msi_BeforeInstall;
+            project.AfterInstall += Msi_AfterInstall;
 
-			project.Load += Msi_Load;
-			project.BeforeInstall += Msi_BeforeInstall;
-			project.AfterInstall += Msi_AfterInstall;
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
 
-			//project.SourceBaseDir = "<input dir path>";
-			//project.OutDir = "<output dir path>";
+            project.BuildMsi();
+        }
 
-			project.BuildMsi();
-		}
+        static void Msi_Load(SetupEventArgs e)
+        {
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "Load");
+        }
 
-		static void Msi_Load(SetupEventArgs e)
-		{
-			if (!e.IsUISupressed && !e.IsUninstalling)
-				MessageBox.Show(e.ToString(), "Load");
-		}
+        static void Msi_BeforeInstall(SetupEventArgs e)
+        {
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "BeforeInstall");
+        }
 
-		static void Msi_BeforeInstall(SetupEventArgs e)
-		{
-			if (!e.IsUISupressed && !e.IsUninstalling)
-				MessageBox.Show(e.ToString(), "BeforeInstall");
-		}
-
-		static void Msi_AfterInstall(SetupEventArgs e)
-		{
-			if (!e.IsUISupressed && !e.IsUninstalling)
-				MessageBox.Show(e.ToString(), "AfterExecute");
-		}
-	}
+        static void Msi_AfterInstall(SetupEventArgs e)
+        {
+            if (!e.IsUISupressed && !e.IsUninstalling)
+                MessageBox.Show(e.ToString(), "AfterExecute");
+        }
+    }
 }
